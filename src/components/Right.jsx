38642cc368e4e67e4b8e29eabe827a5cf137f437
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-01-31 22:32:34 +0500

    Removing error

--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -4,9 +4,7 @@ const Right = () => {
   return (
     <div className="bg-red-400">
       <h1>Something Cool</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur 
-      </p>
+      <p>Lorem ipsum</p>
     </div>
   );
 };
